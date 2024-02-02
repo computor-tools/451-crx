60fe486d6a93a5ee9c5cb110b192fb628ba73084
alexmf91 <alexmunoz91@gmail.com>
2024-02-02 20:52:38 +0100

    Add draft Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+	return (
+		<header class="p-5 shadow-md flex justify-between w-full">
+			<span class="text-3xl font-bold">Qx</span>
+			<img
+				src="icons/full-black-logo.svg"
+				alt="qubic-logo"
+				width={100}
+				height={100}
+			/>
+		</header>
+	);
+}
