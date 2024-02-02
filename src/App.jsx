d3e8fc4556c452663446c35b4a5e1fc31968fcd0
alexmf91 <alexmunoz91@gmail.com>
2024-02-02 20:54:51 +0100

    Update App component: - Add draft content

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import styles from './App.module.css';
+import { Header } from './components';
 
 function App() {
-  return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-      </header>
-    </div>
-  );
+	return (
+		<div class="min-w-[800px] min-h-[500px]">
+			<Header />
+			<main class="bg-blue-dashed-bg h-screen w-screen -mt-2"></main>
+		</div>
+	);
 }
 
 export default App;
