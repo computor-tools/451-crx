efee1390b50b465a7a6486b5b4eaeee42322e29c
alexmf91 <alexmunoz91@gmail.com>
2024-02-03 21:33:12 +0100

    Remove blue dashed bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@
 export default {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {
-			backgroundImage: {
-				'blue-dashed-bg': "url('/img/blue-dashed-bg.svg')",
-			},
-		},
+		extend: {},
 	},
 	plugins: [],
 };
