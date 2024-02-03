108732377b00bc86642c62edae3b485c89546b33
alexmf91 <alexmunoz91@gmail.com>
2024-02-03 21:42:08 +0100

    Update icon to 451 icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,7 @@ export default function Header() {
 	return (
 		<header class="p-5 shadow-md flex justify-between w-full">
 			<span class="text-3xl font-bold">Qx</span>
-			<img
-				src="icons/full-black-logo.svg"
-				alt="qubic-logo"
-				width={100}
-				height={100}
-			/>
+			<img src="icons/icon.png" alt="qubic-logo" width={40} height={40} />
 		</header>
 	);
 }
