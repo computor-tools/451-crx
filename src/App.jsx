9a671e1daeb46fa0b57ad4727858ea037d5fe288
alexmf91 <alexmunoz91@gmail.com>
2024-02-03 21:42:36 +0100

    Remove blue dashed bg

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 	return (
 		<div class="min-w-[800px] min-h-[500px]">
 			<Header />
-			<main class="bg-blue-dashed-bg h-screen w-screen -mt-2">
+			<main class="h-screen w-screen -mt-2">
 				<div class="flex justify-center items-center h-full">
 					<h1 class="text-3xl font-bold text-blue-dashed-text">
 						Welcome to Qx
