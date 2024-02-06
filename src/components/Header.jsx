959a76ffb4d4c43109605953d47c0c7b6d14e356
alexmf91 <alexmunoz91@gmail.com>
2024-02-06 09:46:56 +0100

    Update Header component: - Remove qx - Adjust padding - Add height var

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,7 @@
 export default function Header() {
 	return (
-		<header class="p-5 shadow-md flex justify-between w-full">
-			<span class="text-3xl font-bold">Qx</span>
-			<img src="icons/icon.png" alt="qubic-logo" width={40} height={40} />
+		<header class="px-5 py-2 shadow-md flex justify-end w-full h-[var(--header-height)]">
+			<img src="icons/icon.png" alt="451-logo" width={28} height={24} />
 		</header>
 	);
 }
