06a1f684b0bbea5ef31b3c630baeff60f291d76d
Alex Muñoz <77585600+alexmf91@users.noreply.github.com>
2024-02-07 04:19:01 +0100

    feat: tailwindcss setup (#2)
    * Install tailwind and peer depedencies
    
    * Add tailwind config
    
    * Update index.css:
    - Add tailwind directives
    
    * Relocate 451 icon
    
    * Add qubic full black logo
    
    * Add blue dashed bg
    
    * Remove App.module.css
    
    * Add .editorconfig
    
    * Update icons path
    
    * Update favicon
    
    * Add draft Header component
    
    * Update App component:
    - Add draft content
    
    * Add draft content
    
    * Remove blue dashed bg
    
    * Remove qubic full black logo
    
    * Update icon to 451 icon
    
    * Remove blue dashed bg
    
    * Add NotoSans fonts
    
    * Update index.css:
    - Add noto sans fonts
    - Add html noto sants font family
    
    * Update tailwind.config.js:
    - Add Noto sans as primary font
    
    * Relocate Noto fonts in to Noto folder
    
    * Add OFL file to Noto folder
    
    * Update noto paths
    
    * Add root style variables
    
    * Update layout sizes to use css vars
    
    * Update Header component:
    - Remove qx
    - Adjust padding
    - Add height var

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import styles from './App.module.css';
+import { Header } from './components';
 
 function App() {
-  return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-      </header>
-    </div>
-  );
+	return (
+		<div class="w-[var(--vw)] h-[var(--vh)] overflow-hidden">
+			<Header />
+			<main class="h-[var(--container-height)] w-full">
+				<div class="flex justify-center items-center h-full">
+					<h1 class="text-3xl font-bold text-blue-dashed-text">
+						Welcome to Qx
+					</h1>
+				</div>
+			</main>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+	return (
+		<header class="px-5 py-2 shadow-md flex justify-end w-full h-[var(--header-height)]">
+			<img src="icons/icon.png" alt="451-logo" width={28} height={24} />
+		</header>
+	);
+}
