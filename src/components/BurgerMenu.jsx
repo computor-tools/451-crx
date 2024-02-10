c0366cd36431e909cec92d10cd2320ae2cb22747
alexmf91 <alexmunoz91@gmail.com>
2024-02-10 20:08:59 +0100

    Refactor BurgerMenu component layout

--- src/components/BurgerMenu.jsx
+++ src/components/BurgerMenu.jsx
@@ -23,21 +23,25 @@ export default function BurgerMenu() {
 			</button>
 
 			{/* Sidebar */}
-			<aside
+			<div
 				class={`fixed top-0 left-0 w-64 h-full transform transition-transform duration-300 ease-in-out ${
 					isOpen() ? 'translate-x-0' : '-translate-x-full'
 				} bg-surface-container text-on-surface font-primary`}
 			>
 				<div class="p-5">
-					<div class="flex justify-between gap-6">
+					<div class="grid">
+						<button
+							onclick={() => setIsOpen(!isOpen())}
+							class="h-5 w-5 justify-self-end"
+						>
+							<CloseIcon stroke="white" />
+						</button>
 						<Input
+							className="w-[80%]"
 							placeholder="search"
 							color="secondary"
 							size="small"
 						/>
-						<button onclick={() => setIsOpen(!isOpen())}>
-							<CloseIcon />
-						</button>
 					</div>
 					<ul class="mt-4">
 						{MENU_ITEMS.map((item) => (
@@ -52,7 +56,7 @@ export default function BurgerMenu() {
 						))}
 					</ul>
 				</div>
-			</aside>
+			</div>
 		</div>
 	);
 }
