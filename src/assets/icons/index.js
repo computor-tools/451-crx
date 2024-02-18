b14f514008d4ab4c08f2496e405e497e4f882625
alexmf91 <alexmunoz91@gmail.com>
2024-02-18 12:29:54 +0100

    Add new icons to index.js

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,3 +1,5 @@
+export { default as ArrowLeftIcon } from './arrow-left.svg';
+export { default as ClipboardDocumentIcon } from './clipboard-document.svg';
 export { default as CloseIcon } from './close.svg';
 export { default as ExclamationCircleIcon } from './exclamation-circle.svg';
 export { default as HamburgerIcon } from './hamburger.svg';
