4f24f3ba7f33dc35a1f547b7f469e431b098c48d
alexmf91 <alexmunoz91@gmail.com>
2024-02-18 12:30:10 +0100

    Add Home component

---
+++ src/views/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+	return (
+		<div class="flex justify-center items-center h-full">
+			<h1 class="text-3xl font-bold text-blue-dashed-text">
+				Welcome to Qx
+			</h1>
+		</div>
+	);
+}
