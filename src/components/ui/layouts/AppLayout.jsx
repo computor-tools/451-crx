f98c39a5d7716de89aa789cc70c8dcd144608e2b
alexmf91 <alexmunoz91@gmail.com>
2024-02-18 13:18:54 +0100

    Add props documentation to AppLayout component

--- src/components/ui/layouts/AppLayout.jsx
+++ src/components/ui/layouts/AppLayout.jsx
@@ -1,5 +1,10 @@
 import { Header } from '@/components';
 
+/**
+ * AppLayout component props documentation
+ * @param {Object} props - Component props
+ * @param {Solid.Children} props.children - The content to be rendered within the main area of the layout.
+ */
 export default function AppLayout(props) {
 	return (
 		<div class="w-[var(--vw)] h-[var(--vh)]">
