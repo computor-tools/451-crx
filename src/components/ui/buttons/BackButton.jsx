ebde95fe1f11de8f24d560bb179e5608114d062c
alexmf91 <alexmunoz91@gmail.com>
2024-02-18 13:19:08 +0100

    Add BackButton component

---
+++ src/components/ui/buttons/BackButton.jsx
@@ -0,0 +1,16 @@
+import { ArrowLeftIcon } from '@/assets/icons';
+import { A } from '@solidjs/router';
+import { twMerge } from 'tailwind-merge';
+
+/**
+ * BackButton component props documentation
+ * @param {Object} props - Component props
+ * @param {string} [props.class=''] - Additional CSS classes for custom styling.
+ */
+export default function BackButton(props) {
+	return (
+		<A href="/" class={twMerge('absolute left-10 top-16', props.class)}>
+			<ArrowLeftIcon class="h-6 w-6" />
+		</A>
+	);
+}
