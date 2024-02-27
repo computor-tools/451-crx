0ef58c2f08cdc32cb66df83f2562ff6f59a28eb1
alexmf91 <alexmunoz91@gmail.com>
2024-02-27 17:36:40 +0100

    Update Header component with ConnectionStatus

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import BurgerMenu from './BurgerMenu';
+import ConnectionStatus from './ConnectionStatus';
 
 export default function Header() {
 	return (
-		<header class="px-5 py-2 shadow-md flex justify-between w-full h-[var(--header-height)]">
+		<header class="px-5 py-2 shadow flex justify-between w-full h-[var(--header-height)]">
 			<BurgerMenu />
 			<img src="icons/icon.png" alt="451-logo" width={28} height={24} />
+			<ConnectionStatus status="Synced" />
 		</header>
 	);
 }
