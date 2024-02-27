ecdca021ddad201d7abe492e1319da0d2e6bc8a9
alexmf91 <alexmunoz91@gmail.com>
2024-02-27 18:50:18 +0100

    Refactor Home.jsx to remove unnecessary CSS class

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,7 +2,7 @@ import { AssetsTable, NavBar } from '@/components';
 
 export default function Home() {
 	return (
-		<div class="flex flex-col gap-10 justify-center items-center h-full">
+		<div class="flex flex-col gap-10 justify-center items-center">
 			<NavBar />
 			<AssetsTable />
 		</div>
