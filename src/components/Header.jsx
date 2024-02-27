01fe290d5be53f158d457799d924819a85c27d72
alexmf91 <alexmunoz91@gmail.com>
2024-02-27 18:50:13 +0100

    Add fixed positioning and background color to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import ConnectionStatus from './ConnectionStatus';
 
 export default function Header() {
 	return (
-		<header class="px-5 py-2 shadow flex justify-between w-full h-[var(--header-height)]">
+		<header class="px-5 py-2 shadow flex justify-between w-full h-[var(--header-height)] fixed top-0 bg-white z-10">
 			<BurgerMenu />
 			<img src="icons/icon.png" alt="451-logo" width={28} height={24} />
 			<ConnectionStatus status="Synced" />
