c88ffcdcb068b9d916e76f66dc299d792d256e37
alexmf91 <alexmunoz91@gmail.com>
2024-03-08 17:45:19 +0100

    Refactor Header component to display formatted energy value

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,6 @@
-import { Show } from 'solid-js';
+import { formatQubic } from '@/utils/units';
 import { useLocation } from '@solidjs/router';
+import { Show } from 'solid-js';
 import BurgerMenu from './BurgerMenu';
 import NetworkStatus from './NetworkStatus';
 import TxInfo from './TxInfo';
@@ -13,7 +14,9 @@ export default function Header() {
         <>
             <header class="px-5 py-2 shadow flex justify-between w-[var(--vw)] h-[var(--header-height)] fixed top-0 bg-white z-10">
                 <BurgerMenu />
-                <Show when={entity()?.energy !== undefined}>{entity().energy.toLocaleString()}Qus</Show>
+                <Show when={entity()?.energy !== undefined}>
+                    <span class="mx-auto">{formatQubic(entity().energy)}</span>
+                </Show>
                 <NetworkStatus />
             </header>
             {location.pathname === '/' && <TxInfo />}
