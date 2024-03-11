0caeb290a015659158aeff3d008c3e447c4fa54f
alexmf91 <alexmunoz91@gmail.com>
2024-03-11 17:53:18 +0100

    Add constants

---
+++ src/constants/index.js
@@ -0,0 +1,32 @@
+export const CONTRACT_DESCRIPTIONS = [
+    {
+        name: '',
+        index: 0,
+        constructionEpoch: 0,
+        destructionEpoch: 0,
+    },
+    {
+        name: 'Qx',
+        index: 1,
+        constructionEpoch: 66,
+        destructionEpoch: 10000,
+    },
+    {
+        name: 'QTRY',
+        index: 2,
+        constructionEpoch: 72,
+        destructionEpoch: 10000,
+    },
+    {
+        name: 'RANDOM',
+        index: 3,
+        constructionEpoch: 88,
+        destructionEpoch: 10000,
+    },
+    {
+        name: 'QUTIL',
+        index: 4,
+        constructionEpoch: 97,
+        destructionEpoch: 10000,
+    },
+];
