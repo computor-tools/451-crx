0648ff2dcd8f5d8965832f8b72e290f0d312c95e
alexmf91 <alexmunoz91@gmail.com>
2024-03-19 12:41:42 +0100

    Update background.js imports

--- background.js
+++ background.js
@@ -1,22 +1,2 @@
-console.log('Hello from background.js');
-
-chrome.runtime.onConnect.addListener(function (port) {
-    console.log('Port connected:', port);
-
-    if (port.name === 'popup') {
-        port.onDisconnect.addListener(function () {
-            console.log('Popup closed');
-            chrome.tabs.query({ active: true }, function (tabs) {
-                if (tabs.length === 0) {
-                    navigator.serviceWorker.ready
-                        .then(function (registration) {
-                            registration.active.postMessage({ command: 'LOGOUT' });
-                        })
-                        .catch(function (error) {
-                            console.error('Service worker not ready:', error);
-                        });
-                }
-            });
-        });
-    }
-});
+import './scripts/chrome.js';
+import './scripts/lrv.js';
