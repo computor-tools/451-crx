87fe04f75a45f8fa0099d4c1152c3b34e16020ad
alexmf91 <alexmunoz91@gmail.com>
2024-04-08 16:44:14 +0200

    Update Header component styles to support dark mode

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -12,7 +12,7 @@ export default function Header() {
 
     return (
         <>
-            <header class="px-5 py-2 shadow flex justify-between w-[var(--vw)] h-[var(--header-height)] fixed top-0 bg-white z-10">
+            <header class="px-5 py-2 shadow flex justify-between w-[var(--vw)] h-[var(--header-height)] fixed top-0 bg-white z-10 dark:bg-surface dark:border-b dark:border-zinc-600">
                 <BurgerMenu />
                 <Show when={entity()?.energy !== undefined}>
                     <span class="mx-auto absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">{formatQubic(entity().energy)}</span>
