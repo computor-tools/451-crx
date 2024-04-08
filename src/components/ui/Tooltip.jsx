8bacd0c0ac9084916044d685710b33cd555a35f8
alexmf91 <alexmunoz91@gmail.com>
2024-04-08 16:48:06 +0200

    Update tooltip styling

--- src/components/ui/Tooltip.jsx
+++ src/components/ui/Tooltip.jsx
@@ -12,7 +12,7 @@ export default function Tooltip(props) {
             </div>
             <Show when={shouldShowTooltip()}>
                 <div class="absolute bottom-full left-1/2 mb-2 -translate-x-1/2 transform">
-                    <div class="whitespace-nowrap rounded-lg bg-gray-700 px-2 py-1 text-sm text-white">{props.content}</div>
+                    <div class="whitespace-nowrap rounded-lg bg-zinc-700 px-2 py-1 text-xs text-white">{props.content}</div>
                     <svg class="absolute left-1/2 top-full h-3 w-full -translate-x-1/2 transform text-gray-700" x="0px" y="0px" viewBox="0 0 255 255" xmlSpace="preserve">
                         <polygon class="fill-current" points="0,0 127.5,127.5 255,0" />
                     </svg>
