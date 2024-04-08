84b15a6af123abc44f2732760c6413ebf6bb26d0
alexmf91 <alexmunoz91@gmail.com>
2024-04-08 16:43:33 +0200

    Update NetworkStatus component styling to support dark mode

--- src/components/Header/NetworkStatus.jsx
+++ src/components/Header/NetworkStatus.jsx
@@ -13,8 +13,8 @@ const appendTickAndEpoch = function (epoch, tick) {
 export default function NetworkStatus() {
     return (
         <div class="flex items-center justify-end gap-2">
-            <Show when={isNetworkConnected()} fallback={<p>Connecting...</p>}>
-                <div class="text-xxs grid place-items-end">
+            <Show when={isNetworkConnected()} fallback={<p class="dark:text-gray-200">Connecting...</p>}>
+                <div class="text-xxs grid place-items-end dark:text-gray-200">
                     <span>3/4 peers</span>
                     <span>{appendTickAndEpoch(broadcastedComputors()?.epoch, tick()?.tick)}</span>
                 </div>
